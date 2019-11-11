01b6003f905f1545a0fcf325f144fb0791912c92
Andrzej Mazur <andrzej@mazur.info>
2019-11-11 15:42:56 +0100

    client updated according to udemy course

--- client.cpp
+++ client.cpp
@@ -1,40 +1,45 @@
 /****************** CLIENT CODE ****************/
 
 #include <stdio.h>
-#include <arpa/inet.h> 
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netinet/in.h>
-#include <string.h>
 
 int main(){
-  int clientSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(7891);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+  int network_socket; //socket descriptor
+  int connection_status; //stores socket connection status
 
-  /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  /* create a socket with the following configuration
+     domain:AFINET: IPV4 address space (AFINET6 can be used for IPV6)
+     type:SOCK_STREAM: Sequenced, reliable, connection-based byte streams
+     protocol: 0: select protocol automatically
+  */ 
+  network_socket=socket(AF_INET,SOCK_STREAM,0);
 
-  /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+struct sockaddr_in server_address;
 
-  /*---- Print the received message ----*/
-  printf("Data received: %s",buffer);   
+/* Configure settings of the server address struct */
+/* Address family = Internet */
+server_address.sin_family = AF_INET;
+/* Set port number, using htons function to use proper byte order */
+server_address.sin_port = htons(9092);
+/* Set IP address to localhost */
+server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+if (connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address))== -1) {
+  printf("Error when connecting to socket...");
+  return 1;
+}
+
+/* receive data from server */
+char receive_buffer [255];
+
+recv(network_socket,&receive_buffer,sizeof(receive_buffer),0);
 
   return 0;
 }
