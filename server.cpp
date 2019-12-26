2ea60ee20583e7b06e7a0c8eee1643f7fb1cc7af
Andrzej Mazur <andrzej@mazur.info>
2019-12-26 21:08:20 +0100

    debug messages added

--- server.cpp
+++ server.cpp
@@ -23,6 +23,9 @@
 /****************************** SERVER CODE ********************************/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -30,53 +33,191 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+//enable debuging by setting DEBUG to 1
+
+
+/*
+Debug macro definition
+
+The do { ... } while (0) idiom ensures that the code acts like a statement (function call). 
+The unconditional use of the code ensures that the compiler always checks that your debug 
+code is valid — but the optimizer will remove the code when DEBUG is 0.
+"fmt" is a printf format string. "..." is whatever extra arguments fmt requires 
+(possibly nothing).
+__VA_ARGS__ - means variable arguments
+##__VA_ARGS__ - comma before the ‘##’ will be deleted. This does not happen if 
+empty argument is passed, nor does it happen if the token preceding ‘##’ 
+is anything other than a comma. 
+see https://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html for more info
+*/
+
+//#define DEBUG
+
+#ifdef DEBUG
+#define debug_print(fmt, ...) \
+    fprintf(stderr, "%s() in %s, line %i: " fmt "\n", \
+        __func__, __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug_print(fmt, ...) ((void)0)
+#endif
+
+
 #define PROGRAM_NAME "server"
+#define PROGRAM_VERSION "1.0.0"
+
+struct helptxt {
+  const char *opt;
+  const char *desc;
+};
+
+static const struct helptxt helptext[] = {
+  {"-h, --help",
+   "Display this help message"},
+  {"-v, --version",
+   "Display version number and quit"},
+  {"-s, --sleep-interval <microseconds>",
+   "Set message send interval in microseconds, if not specified 5000000 microseconds is used"},
+  { NULL, NULL }
+};
+
+void help(void)
+{
+  int i;
+  printf("Usage:\n");
+  printf("./%s [options]\n",PROGRAM_NAME);
+    printf("     where option include values:\n");
+  for(i = 0; helptext[i].opt; i++) {
+    printf("           %-36s %s\n", helptext[i].opt, helptext[i].desc);
+  }
+  printf("\n");
+  printf("Mail bug reports and suggestions to <andrzej@mazur.info>\n");
+}
+
+void version(void)
+{
+  printf("%s ver. %s\n",PROGRAM_NAME, PROGRAM_VERSION);
+  printf("\n");
+}
+
+typedef struct CommandLineParameters_ {
+   unsigned int sleepInterval;
+   bool displayHelp;
+   bool displayVersion;
+   bool error;
+} CommandLineParameters;
+
+static CommandLineParameters parseParameters(int argc, char** argv) {
+
+  CommandLineParameters flags = {
+    .sleepInterval=5000000, //default sleep time set 5000000 microseconds
+    .displayHelp=false,
+    .displayVersion=false,
+    .error=false,
+  };
+
+  if (argc==1){
+   // program called with no parameters, use defeault settings
+    return flags;
+  }else
+  {
+   //parse supplied parameters, omit the first one since its a program name
+   int i;
+    char currentParamStr[20];
+
+   debug_print("argc=%i\n", argc);
+   for(i=1;i<argc;i++)
+    {
+     debug_print("parsed parameter:%s\n",argv[i]);
+
+     if (sscanf(argv[i], "%s", &currentParamStr)!=1){
+        /* error - converting supplied parameter into string */
+        debug_print("error - converting supplied parameter into string\n");
+        flags.error=true;
+        return flags;
+      }
+
+      if ((strcmp(currentParamStr,"-h")==0) || (strcmp(currentParamStr,"--help")==0))
+      {
+        if (argc>2)
+        {
+          /*error - help parameter called not alone*/
+          debug_print("error - help parameter called not alone\n");
+          flags.error=true;
+          return flags;
+        }
+        flags.displayHelp=true;
+      } 
+      else 
+      { 
+        if ((strcmp(currentParamStr,"-v")==0) || (strcmp(currentParamStr,"--version")==0))
+        {
+          if (argc>2)
+          {
+            /*error - version parameter called not alone*/
+            debug_print("error - version parameter called not alone\n");
+            flags.error=true;
+            return flags;
+          }
+          flags.displayVersion=true;
+        } 
+        else
+        { 
+          /*error - unspecified parameter provided*/
+          debug_print("error - unspecified parameter provided\n");
+          flags.error=true;
+          return flags;
+        }
+      }
+   }
+  return flags;
+  }
+}
+
+typedef struct Parameters_ {
+   unsigned int sleepInterval;
+} Parameters;
+
+ Parameters SetConfiguredParametersOrQuit(CommandLineParameters flags){
+
+   Parameters ProgramRunTimeParameters;
+
+  /*check for errors to be done first - other flags not relevant if error = true*/
+  if (flags.error==true){
+    printf("%s: Supplied incorrect parameter(s)...\n\n",PROGRAM_NAME);
+    help();
+    exit(1);
+  }
+
+  if (flags.displayHelp==true)
+  {
+    help();
+    exit(0);
+  }
+
+  if (flags.displayVersion==true)
+  {
+    version();
+    exit(0);
+  }
+
+  ProgramRunTimeParameters.sleepInterval=flags.sleepInterval;
+  return ProgramRunTimeParameters;
+
+}
 
 int main(int argc, char *argv[]){
 
-  pid_t pid = getpid();
-  printf("%s: process:pid=%lu has started!\n",PROGRAM_NAME,(long)pid);
-
- 
+  Parameters RunTimeParameters;
   unsigned int sleep_interval; //sleep interval in microseconds
 
-  switch(argc){
-        /*
-    program param1 param2 ... paramn
-    argv[0] - program
-    argv[1] - param1
-    argv[2] - param2
-    ...
-    argv[n] - paramn
-    */
-    case 1:
-      //program called with no parameters, use default sleep time
-      sleep_interval=5000000; //sleep interval in microseconds
-      break;
-    case 2:
-      //program called with one parameter
-      if (sscanf(argv[1], "%u", &sleep_interval)!=1){
-        /* error converting supplied parameter into unsigned integer */
+   
+  RunTimeParameters=SetConfiguredParametersOrQuit(parseParameters(argc,argv));
+  sleep_interval=RunTimeParameters.sleepInterval;
 
-        printf("%s: Supplied parameter was incorrect...\n",PROGRAM_NAME);
-        printf("\n");
-        printf("Usage:\n");
-        printf("./%s [options]\n",PROGRAM_NAME);
-        printf("    where options include values:\n");
-        printf("        -h or --help           -print this message\n");
-        printf("        -v or --version        -print version of this program\n");
-        printf("        -sleep <microseconds>  -set send interval for messages sent to clients\n");
-        printf("\n");
-        printf("Mail bug reports and suggestions to <andrzej@mazur.info>\n");
-        return 1;
-      }
-      break;
-    default:
-      printf("Incorrect number of parameters! Exiting the program ...\n");
-      return 1;
-  }
+  pid_t pid = getpid();
+  printf("%s: Process:pid=%lu has started!\n",PROGRAM_NAME,(long)pid);
 
-printf("Sleep interval has been set to: %u\n", sleep_interval);
+  debug_print("Sleep interval has been set to: %u\n", sleep_interval);
  
 
   int server_socket; //socket descriptor
@@ -102,18 +243,18 @@ server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 /* Bind the socket to specified IP address and port */ 
 if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address))== -1) {
-  printf("Error when binding socket to IP address and port...\n");
+  printf("%s: Error when binding socket to IP address and port...\n",PROGRAM_NAME);
   return 1;
 }/* code */
 
 /* Prepare for connections from clients */
 /* Second argument indicates how many connections are qued before they are becoming rejected by the socket */
 if (listen (server_socket, 5) == -1){/* code */
-  printf("Incomming connnection has been refused...\n");
+  printf("%s: Incomming connnection has been refused...\n",PROGRAM_NAME);
   return 1;
   }else
   {
-    printf("Successfuly prepared to listen from clients and now awaiting connection from client\n");
+    printf("%s: Successfuly prepared to listen from clients and now awaiting connection from client\n",PROGRAM_NAME);
   }
   
   int client_socket;
@@ -126,12 +267,12 @@ if (listen (server_socket, 5) == -1){/* code */
 client_socket=accept(server_socket, (struct sockaddr *) &client_address, (unsigned int *) &client_address_len);
 
 if (client_socket==-1){
-  printf("Error when connection from server has been received...\n");
+  printf("%s: Error when connection from server has been received...\n",PROGRAM_NAME);
   return 1;
 }else{
-  printf("Connection successfuly recieved, client socket has been opened\n");
-  printf("  Client IP: %s\n", inet_ntoa(client_address.sin_addr));
-  printf("  Client Port: %hu\n", ntohs(client_address.sin_port));
+  printf("%s: Connection successfuly recieved, client socket has been opened\n",PROGRAM_NAME);
+  printf("%s:   Client IP: %s\n",PROGRAM_NAME, inet_ntoa(client_address.sin_addr));
+  printf("%s:   Client Port: %hu\n",PROGRAM_NAME, ntohs(client_address.sin_port));
 }
 
 //printf("Press <ENTER> to stop the server application\n");
@@ -143,12 +284,12 @@ while (idx<3){
   sprintf(server_msg,"Msg no.: %d: <-------Hallo from the Server--------->",idx);
   /* Send a message from server to client */
   if(send(client_socket,server_msg,sizeof(server_msg),0)==-1){
-    printf("Sending data to the client has not been successful...\n");
+    printf("%s: Sending data to the client has not been successful...\n",PROGRAM_NAME);
     return 1;
   } else
   {
-    printf("Message #%d, successfuly send to the client\n",idx);
-    printf("Next message will be sent to the client in %d microseconds\n",sleep_interval);
+    printf("%s: Message #%d, successfuly send to the client\n",PROGRAM_NAME,idx);
+    printf("%s: Next message will be sent to the client in %d microseconds\n",PROGRAM_NAME,sleep_interval);
     usleep(sleep_interval);
     idx++;
   }
